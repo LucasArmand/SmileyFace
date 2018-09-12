a567657b691e48a7d7c629739ac251abd4c8ba6b
LucasArmand <40708637+LucasArmand@users.noreply.github.com>
2018-09-12 13:03:29 -0400

    Update Face.java

--- Face.java
+++ Face.java
@@ -3,7 +3,6 @@
 public class Face {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.print("/////////\n");
 		System.out.print("| *   * |\n");
 		System.out.print("|   *   |\n");
