c2a799934b97fdad3648c45f81e7ddcd38d88a93
LucasArmand <40708637+LucasArmand@users.noreply.github.com>
2018-09-12 13:03:00 -0400

    Add files via upload
    Initial Commit

---
+++ Face.java
@@ -0,0 +1,14 @@
+// Lucas Armand
+// Smiley Face
+public class Face {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("/////////\n");
+		System.out.print("| *   * |\n");
+		System.out.print("|   *   |\n");
+		System.out.print("|  \\_/  |\n");
+		System.out.print("\\_______/\n");
+	}
+
+}
